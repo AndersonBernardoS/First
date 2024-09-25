2e0f0a89c5fcce3ebd9dac69ef21ca1a06d6cde3
Anderson Bernardo <anderson.dim.abs@gmail.com>
2024-09-25 09:15:57 -0300

    Add class. Convert data.

---
+++ src/Condicional.java
@@ -0,0 +1,13 @@
+public class Condicional {
+    public static void main(String[] args) {
+        int anoDeLancamento = 2020;
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+
+        if (anoDeLancamento > 2022) {
+            System.out.println("lançamento que clientes curtem.");
+        } else {
+            System.out.println("Filme retrô que vale a pena assistir");
+        }
+    }
+}
